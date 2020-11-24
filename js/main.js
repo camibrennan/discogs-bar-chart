982cbc6707459388e2522d9a1a69a2706567709a
Camelia Brennan <cameliabrennan@gmail.com>
2020-11-24 12:11:57 -0500

    added record image

---
+++ js/main.js
@@ -0,0 +1,44 @@
+
+function mergeList() {
+    console.log('isthisworking')
+    alert('Merge Records: 98 records'); 
+
+}
+
+function beggarsList() {
+    console.log('isthisworking')
+    alert('Beggars: 24 records'); 
+
+}
+
+function barsukList() {
+    console.log('isthisworking')
+    alert('Barsuk: 8'); 
+
+}
+
+function dominoList() {
+    console.log('isthisworking')
+    alert('Domino Recording Co: 46'); 
+
+}
+
+function jadetreeList() {
+    console.log('isthisworking')
+    alert('Jade Tree: 13 records'); 
+
+}
+
+function secretlyList() {
+    console.log('isthisworking')
+    alert('Secretly Canadian Label Group: 8'); 
+
+}
+
+function saddlecreekList() {
+    console.log('isthisworking')
+    alert('Saddle Creek: 23 records'); 
+
+}
+
+
